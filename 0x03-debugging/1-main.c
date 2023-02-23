ea23a81b7f37a54b23be4e7bc527510253ebb3c5
https://github.com/SbuKubheka/alx-system_engineering-devops.git <sbukubheka69@gmail.com>
2023-02-23 15:33:29 -0800

    infinte loop debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - cause an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*{*/
+
+	prinf("infinte loop voided! \\o/\n");
+
+	return (0);
+}
