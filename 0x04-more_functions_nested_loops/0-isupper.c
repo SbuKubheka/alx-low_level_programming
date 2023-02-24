d19d37e01590f42d4ad9dcbb169341719b0f673d
https://github.com/SbuKubheka/alx-system_engineering-devops.git <sbukubheka69@gmail.com>
2023-02-23 23:35:12 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if letter is upper case
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 otherwise
+ */
+
+int _isupper(int x)
+
+{
+	
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
