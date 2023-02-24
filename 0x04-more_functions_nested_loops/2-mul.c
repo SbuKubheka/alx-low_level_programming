65f0bb3d4eb26c84cbbf916ce121bf5693548d4c
https://github.com/SbuKubheka/alx-system_engineering-devops.git <sbukubheka69@gmail.com>
2023-02-24 00:29:18 -0800

    Function that multilies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int mul - Function that multiplies two integers
+ * @x: The first number to multiply
+ * @y: The second number to multiply with the first  number
+ * Return: The result of multiplication between x parameter and y parameter
+ */
+
+int mul(int x, int y)
+
+{
+	
+	return (x * y);
+
+}
